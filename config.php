7d53b6eb3a6927c4aadd6596a953f914ba9a3458
Nicholas Penree <drudge@conceitedsoftware.com>
2003-04-21 16:33:09 +0000

    - server uses stealth.net

--- config.php
+++ config.php
@@ -25,5 +25,5 @@ $config['alt_nick'] = 'phpb1tch';
 $config['version'] = 'phpbitch 1.0 | portions by TiX';
 
 $config['irc_server']="irc.stealth.net";
-$config['irc_port']="6667";
+$config['irc_port']="5555";
 ?>
