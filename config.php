0aeea23e54c2aa2f1be4ce7cb14d6de445a237e9
Mirco Bauer <meebey@meebey.net>
2003-05-04 21:23:06 +0000

    - added php-gtk to $config['friend_bots']. - changed static fullpath modules to fullpath + relative path. - changed database login info. - removed TiX from CTCP Version reply.

--- config.php
+++ config.php
@@ -9,23 +9,23 @@
 /* database settings */
 
 $mysql_server = 'localhost';
-$mysql_username = 'gtkbitch';
+$mysql_username = 'phpbitch';
 $mysql_password = 'php';
-$mysql_db = 'gtkbitch';
+$mysql_db = 'phpbitch';
 
 /* general settings */
 
 $config = array();
 
 $config['real_name'] = 'IRC service bot for #php-gtk';
-$config['channels'] = array('#php-cafe','#php-gtk');
+$config['channels'] = array('#php-gtk');
 $config['nick'] = 'phpbitch';
 $config['ident'] = 'phpbitch';
 $config['alt_nick'] = 'phpb1tch';
 
 /* bot specific */
-$config['version'] = 'phpbitch 1.0 | portions by TiX';
-$config['modules_path'] = '/home/drudge/phpbitch/repository/phpbitch/modules';
+$config['version'] = 'phpbitch 1.0';
+$config['modules_path'] = dirname(__FILE__).'/modules';
 $config['answer_questions'] = true;
 
 /* irc server info */
@@ -33,5 +33,5 @@ $config['irc_server']="irc.stealth.net";
 $config['irc_port']="5555";
 
 // friends go here, the order is the priority for mastermode
-$config['friend_bots'] = array('phpbitch','gtkbitch');
+$config['friend_bots'] = array('phpbitch','gtkbitch', 'php-gtk');
 ?>
