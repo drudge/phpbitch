dd74ad3f19ee63bf06fd31c5c50cdf29fb0ca9c7
Mirco Bauer <meebey@meebey.net>
2003-05-05 14:58:26 +0000

    - added GPL license header. - changed adduser() so ident is also used.

--- modules/users.php
+++ modules/users.php
@@ -4,13 +4,32 @@
  * $Revision$
  * $Author$
  * $Date$
+ *
+ * Copyright (c) 2003 Nicholas 'DrUDgE' Penree <drudge@x-php.net>
+ *                    Mirco 'meebey' Bauer <mail@php.net>
+ *
+ * Full GPL License: <http://www.gnu.org/licenses/gpl.txt>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  */
  
 class Net_SmartIRC_module_users
 {
     var $name = 'users';
     var $description = 'this module has commands to add/remove/search bots user database.';
-    var $author = 'Nicholas \'DrUDgE\' Penree <drudge@php-coders.net>';
+    var $author = 'Nicholas \'DrUDgE\' Penree <drudge@php-coders.net>, Mirco \'meebey\' Bauer <meebey@php.net>';
     var $license = 'GPL';
     
     var $actionid1;
@@ -65,8 +84,9 @@ class Net_SmartIRC_module_users
     {
         global $bot;
         $nick = $data->messageex[1];
-        $host = $data->messageex[2];
-        $level = $data->messageex[3];
+        $ident = $data->messageex[2];
+        $host = $data->messageex[3];
+        $level = $data->messageex[4];
         
         // don't verify ourself
         if (strpos($data->nick, $irc->_nick) !== false) {
@@ -76,11 +96,11 @@ class Net_SmartIRC_module_users
         $result = $bot->reverseverify($irc, $data);
         
         if ($result !== false && ($bot->get_level($result) == USER_LEVEL_MASTER)) {
-            $query = "INSERT INTO dnsentries( `nickname`,`dnsalias`,`level`) VALUES('".$nick."','".$host."','".$level."')";
+            $query = "INSERT INTO dnsentries (nickname,ident,dnsalias,level) VALUES('".$nick."','".$ident."','".$host."','".$level."')";
             $res = $bot->dbquery($query);
             
             if ($res) {
-                $irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, 'Added '.$nick.' as '.$host.' with a level of '.$level);
+                $irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, 'Added '.$nick.' ident: '.$ident.' host: '.$host.' with a level of '.$level);
             } else {
                 $irc->message(SMARTIRC_TYPE_CHANNEL, $data->channel, 'Error adding: '.mysql_error());
             }
