5e2a90853e79a91ddd3ae6f0c759bd1c785e7759
Mirco Bauer <meebey@meebey.net>
2003-05-25 22:52:11 +0000

    - debug info

--- modules/botcmds.php
+++ modules/botcmds.php
@@ -131,7 +131,7 @@ class Net_SmartIRC_module_botcmds
         }
         
         $result = $bot->reverseverify($irc, $data);
-        $irc->message(SMARTIRC_TYPE_CHANNEL, $channel, "result: $result level: $bot->get_level($result)");
+        $irc->message(SMARTIRC_TYPE_CHANNEL, $channel, "result: ".$result." level: ".$bot->get_level($result));
         
         if ($result !== false && ($bot->get_level($result) == USER_LEVEL_MASTER)) {
             $irc->message(SMARTIRC_TYPE_CHANNEL, $channel, trim($message));
