957df97863de5626d04f127d6f8a2ebb27c6060c
Nicholas Penree <drudge@conceitedsoftware.com>
2003-05-25 21:49:30 +0000

    - initial commit.

---
+++ modules/hex_ip.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * $Id$
+ * $Revision$
+ * $Author$
+ * $Date$
+ *
+ * Copyright (c) 2003 Nicholas 'DrUDgE' Penree <drudge@x-php.net>
+ *
+ * Full GPL License: <http://www.gnu.org/licenses/gpl.txt>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ */
+ 
+class Net_SmartIRC_module_hex_ip
+{
+    var $name = 'temp';
+    var $description = 'This module will convert ip2hex and hex2ip.';
+    var $author = 'Nicholas \'DrUDgE\' Penree <drudge@php-coders.net>';
+    var $license = 'GPL';
+    
+    var $actionids = array();
+    
+    function module_init(&$irc)
+    {
+        $this->actionids[] = $irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!temp', $this, 'temp');
+    }
+    
+    function module_exit(&$irc)
+    {
+        foreach ($this->actionids as $value) {
+            $irc->unregisterActionid($value);
+        }
+    }
+    
+    //===============================================================================================
+
+}
+?>
