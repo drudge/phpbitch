61fe5c847562efafd923ff407b380e2a642355c7
Nicholas Penree <drudge@conceitedsoftware.com>
2003-07-16 00:26:51 +0000

    - fixed my fuck up :-/

--- phpbitch.sql
+++ phpbitch.sql
@@ -1,5 +1,5 @@
 
-d: phpbitch_install.sh,v 1.4 2003/07/16 00:01:55 meebey Exp $
+# $Id$
 # $Revision$
 # $Author$
 # $Date$
@@ -21,13 +21,8 @@ d: phpbitch_install.sh,v 1.4 2003/07/16 00:01:55 meebey Exp $
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
-###########################################################################
-# Table structure for table `answering_machine`
-#
-# Creation: May 25, 2003 at 06:26 PM
-# Last update: May 25, 2003 at 06:26 PM
-#
 
+# Table structure for table `answering_machine`
 DROP TABLE IF EXISTS `answering_machine`;
 CREATE TABLE `answering_machine` (
   `to_nick` varchar(9) NOT NULL default '',
@@ -36,15 +31,8 @@ CREATE TABLE `answering_machine` (
   `time` datetime NOT NULL default '0000-00-00 00:00:00',
   `message` text
 ) TYPE=MyISAM;
-# --------------------------------------------------------
 
-#
 # Table structure for table `brain`
-#
-# Creation: Apr 25, 2003 at 03:51 PM
-# Last update: Jul 15, 2003 at 05:16 PM
-#
-
 DROP TABLE IF EXISTS `brain`;
 CREATE TABLE `brain` (
   `query` varchar(100) NOT NULL default '',
@@ -52,15 +40,8 @@ CREATE TABLE `brain` (
   `count` int(11) default '0',
   UNIQUE KEY `query` (`query`)
 ) TYPE=MyISAM;
-# --------------------------------------------------------
 
-#
 # Table structure for table `users`
-#
-# Creation: Jul 13, 2003 at 06:06 PM
-# Last update: Jul 15, 2003 at 02:05 PM
-#
-
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `nickname` varchar(32) NOT NULL default '',
@@ -69,4 +50,3 @@ CREATE TABLE `users` (
   `level` int(11) NOT NULL default '0',
   PRIMARY KEY  (`nickname`,`host`)
 ) TYPE=MyISAM;
-
