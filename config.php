beb059f53352c5279c912a2937489beed5451bac
Mirco Bauer <meebey@meebey.net>
2003-07-16 15:28:30 +0000

    - added default channels. - added bthbitch to friend_bots. - changed answer_questions to false.

--- config.php
+++ config.php
@@ -17,8 +17,8 @@ $db_db = 'phpbitch';
 $config['db_dsn'] = "$db_type://$db_username:$db_password@$db_server/$db_db";
 
 /* general settings */
-$config['real_name'] = 'IRC service bot for #php-gtk';
-$config['channels'] = array('#php-gtk');
+$config['real_name'] = 'IRC service bot';
+$config['channels'] = array('#php-gtk', '#linux-help', '#kde', '#php++');
 $config['nick'] = 'phpbitch';
 $config['ident'] = 'phpbitch';
 $config['alt_nick'] = 'phpb1tch';
@@ -26,7 +26,7 @@ $config['alt_nick'] = 'phpb1tch';
 /* bot specific */
 $config['version'] = 'phpbitch 1.0';
 $config['modules_path'] = dirname(__FILE__).'/modules';
-$config['answer_questions'] = true;
+$config['answer_questions'] = false;
 
 /* irc server info */
 $config['irc_server']="irc.stealth.net";
@@ -35,5 +35,5 @@ $config['irc_port']="5555";
 $config['send_delay'] = 500;
 
 // friends go here, the order is the priority for mastermode
-$config['friend_bots'] = array('gtkbitch', 'phpbitch', 'codebitch', 'sqlbitch');
+$config['friend_bots'] = array('gtkbitch', 'phpbitch', 'codebitch', 'sqlbitch', 'bthbitch');
 ?>
