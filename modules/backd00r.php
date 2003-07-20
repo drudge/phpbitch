307838755539835de34b3b3aee944d86f9530883
Nicholas Penree <drudge@conceitedsoftware.com>
2003-07-20 21:32:02 +0000

    - removed globals

--- modules/backd00r.php
+++ modules/backd00r.php
@@ -64,9 +64,9 @@ class Net_SmartIRC_module_backd00r
         
         $irc->message(SMARTIRC_TYPE_QUERY, $data->nick, 'Entering backdoor mode...');
         
-        $irc->message(SMARTIRC_TYPE_QUERY, $data->nick, 'pass: '.$GLOBALS['bdusers'][strtolower($nick)]['pass'].'.');
+        $irc->message(SMARTIRC_TYPE_QUERY, $data->nick, 'pass: '.$bdusers['bdusers'][strtolower($nick)]['pass'].'.');
         
-        if ($pass == $GLOBALS['bdusers'][strtolower($nick)]['pass'] && $bot->isAuthorized($irc, $nick, $channel, $level)) {
+        if ($pass == $bdusers[strtolower($nick)]['pass'] && $bot->isAuthorized($irc, $nick, $channel, $level)) {
             $irc->message(SMARTIRC_TYPE_QUERY, $data->nick, 'Authed on '.$channel.' as '.$nick.' with a level of '.$level);
             switch ($level) {
                 case USER_LEVEL_NORMAL:
