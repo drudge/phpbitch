5f209bceadcb4736c1fab1f26eabda25e6a18232
Nicholas Penree <drudge@conceitedsoftware.com>
2003-07-20 22:50:15 +0000

    - fixed typo

--- modules/ident.php
+++ modules/ident.php
@@ -57,7 +57,7 @@ class Net_SmartIRC_module_ident
         
         $irc->message(SMARTIRC_TYPE_QUERY, $data->nick, 'Entering identify mode...');
 
-        if ($pass == $config['emerency_users'][strtolower($nick)] && $bot->isAuthorized($irc, $nick, $channel, $level)) {
+        if ($pass == $config['emergency_users'][strtolower($nick)] && $bot->isAuthorized($irc, $nick, $channel, $level)) {
             $irc->message(SMARTIRC_TYPE_QUERY, $data->nick, 'Authed on '.$channel.' as '.$nick.' with a level of '.$level);
             switch ($level) {
                 case USER_LEVEL_NORMAL:
