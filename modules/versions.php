936735086f26a4b4f6ed82dc5360f64264d7df02
Mirco Bauer <meebey@meebey.net>
2003-07-25 16:22:41 +0000

    - first import.

---
+++ modules/versions.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * $Id$
+ * $Revision$
+ * $Author$
+ * $Date$
+ *
+ * Copyright (c) 2003 Mirco 'meebey' Bauer <meebey@php.net>
+ *
+ * Full GPL License: <http://www.gnu.org/licenses/gpl.txt>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ */
+ 
+class Net_SmartIRC_module_versions
+{
+    var $name = 'versions';
+    var $description = 'this module shows the version numbers of different things.';
+    var $author = 'Mirco \'meebey\' Bauer <meebey@php.net>';
+    var $license = 'GPL';
+    var $actionids = array();
+    
+    function module_init(&$irc)
+    {
+        $this->actionids[] = $irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL|SMARTIRC_TYPE_QUERY|SMARTIRC_TYPE_NOTICE, '^!phpversion', $this, 'showphpversion');
+        $this->actionids[] = $irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL|SMARTIRC_TYPE_QUERY|SMARTIRC_TYPE_NOTICE, '^!smartircversion', $this, 'showsmartircversion');
+    }
+    
+    function module_exit(&$irc)
+    {
+        foreach ($this->actionids as $value) {
+            $irc->unregisterActionid($value);
+        }
+    }
+    
+    function showphpversion(&$irc, &$data)
+    {
+        $irc->message($data->type, $data->channel, phpversion());
+    }
+    
+    function showsmartircversion(&$irc, &$data)
+    {
+        $irc->message($data->type, $data->channel, SMARTIRC_VERSION);
+    }
+}
+?>
